85725c4c27cb533069a58480219ac088ba57151e
ongpeipei <73981638+OPPisme@users.noreply.github.com>
2022-04-10 17:42:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+harakiri = 65
+add-header = Connection: Keep-Alive
+http-keepalive = 1
+
 customhost = "employee.cwb46pkzcqhg.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
