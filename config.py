939cbe8befe6b454b97b95f01f6dcacc38588a88
JetWai97 <74056345+JetWai97@users.noreply.github.com>
2022-04-10 17:58:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-harakiri = 65
-add-header = Connection: Keep-Alive
-http-keepalive = 1
-
 customhost = "employee.cwb46pkzcqhg.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
