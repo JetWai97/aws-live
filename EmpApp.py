0a0827b49b9287a125b0f8067fdd01b4bcf15388
jx0905 <74499182+jx0905@users.noreply.github.com>
2022-04-10 22:27:45 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -45,8 +45,8 @@ def dirupdate():
 
 @app.route("/payupdate", methods=['GET','POST'])
 def dirpay():
-    emp_id = request.form.get['emp_id']
-    salary = request.form.get['salary']
+    emp_id = request.form['emp_id']
+    salary = request.form['salary']
 
     updatesql = "UPDATE employee SET salary= %s WHERE emp_id = %s"
     mycursor = db_conn.cursor()
